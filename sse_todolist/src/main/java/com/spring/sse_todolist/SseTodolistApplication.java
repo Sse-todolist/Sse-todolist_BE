5c1f43f580c2d5761b67232714dc5f31dab7449b
lky8967 <lky8967@gmail.com>
2023-05-14 15:56:26 +0900

    첫 커밋

---
+++ sse_todolist/src/main/java/com/spring/sse_todolist/SseTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sse_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseTodolistApplication.class, args);
+	}
+
+}
---
+++ sse_todolist/src/test/java/com/spring/sse_todolist/SseTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sse_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
